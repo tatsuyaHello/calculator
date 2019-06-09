9c96189cc473e95fc9afed4dd16a09356ed197c3
yamamaura_tatusya <t.yamamura@gemcook.com>
2019-06-09 21:26:26 +0900

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by tatsuya yamamura on 2019/06/09.
+//  Copyright © 2019 tatsuya yamamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
